bf3b0e0f771d3066b5798b6498918c8d80d1fcf8
Donal Spring <donalspring@gmail.com>
2016-02-21 18:01:32 +0000

    FIX - integrated from with disable and backend

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,7 +3,8 @@ console.log('\'Allo \'Allo!');
 
 function sendForm() {
   console.log('sendForm :: entered');
-  var response = document.getElementById('form-response')
+  var response = document.getElementById('form-response');
+  var button = document.getElementById('form-button');
 
   var name = document.getElementById('form-name').value;
   var email = document.getElementById('form-email').value;
@@ -11,9 +12,12 @@ function sendForm() {
   var msg = document.getElementById('form-msg').value;
   if (!name || !email || !phone || !msg){
     console.log('Something not set');
-    response.innerHTML = "Please complete all fields.";
+    response.innerHTML = "Please complete all fields";
+    response.style.borderBottomColor = "FIREBRICK";
     return false
   } else {
+    button.innerHTML = '<i class="fa fa-refresh fa-spin"></i> Submitting';
+    button.disabled = true;
     console.log('All things set');
     var request = {
       name :name,
@@ -21,13 +25,20 @@ function sendForm() {
       phone : phone,
       message  : msg
     };
+    //#form-response     .style.color
+
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       console.log('XHR response :: '+xhttp.responseText);
       if (xhttp.readyState == 4 && xhttp.status == 200) {
-        response.innerHTML = "Thanks! I'll be in touch soon."
+        response.innerHTML = "Thanks! I'll be in touch soon";
+        response.style.borderBottomColor = "#00796B";
+        button.innerHTML = 'Submit';
       } else if (xhttp.readyState == 4 && xhttp.status !== 200) {
-        response.innerHTML = "Oops! something went wrong"
+        response.innerHTML = "Oops! Something went wrong";
+        response.style.borderBottomColor = "FIREBRICK";
+        button.innerHTML = 'Submit';
+        button.disabled = false;
       }
     };
     xhttp.open("post", "/contact", true);
